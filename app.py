254e453bd25963082b45fb79e2e133cf12d984fd
Flet4er <geleonrore@gmail.com>
2025-05-14 22:12:26 +0300

    update

--- app.py
+++ app.py
@@ -740,6 +740,8 @@ def download_all_zip():
 
 
 
-
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
+    
+#if __name__ == '__main__':
+#    app.run(debug=True)
